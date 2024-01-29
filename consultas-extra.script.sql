abc999a5dc2d57a028968b1fd1b7ad701f2daf17
patrixampm <patrick.a.peckham@gmail.com>
2024-01-29 13:30:58 +0100

    Primer Commit

---
+++ consultas-extra.script.sql
@@ -0,0 +1,50 @@
+-- Listar las pistas ordenadas por el número de veces que aparecen en playlists de forma descendente
+SELECT	T.Name,
+		COUNT(PT.TrackId) AS VecesEnLista
+FROM Track T
+INNER JOIN PlaylistTrack PT
+ON T.TrackId = PT.TrackId
+GROUP BY T.Name
+ORDER BY VecesEnLista DESC
+
+-- Listar las pistas más compradas (la tabla InvoiceLine tiene los registros de compras)
+SELECT	T.Name,
+		COUNT(I.TrackId) AS VecesComprado
+FROM Track T
+INNER JOIN InvoiceLine I
+ON T.TrackId = I.TrackId
+GROUP BY T.Name
+ORDER BY VecesComprado DESC
+
+-- Listar los artistas más comprados
+SELECT	A.Name,
+		COUNT(I.TrackId) AS VecesComprado
+FROM Artist A
+INNER JOIN Album AL
+ON A.ArtistId = AL.ArtistId
+INNER JOIN Track T
+ON AL.AlbumId = T.AlbumId
+INNER JOIN InvoiceLine I
+ON T.TrackId = I.TrackId
+GROUP BY A.Name
+ORDER BY VecesComprado DESC
+
+-- Listar las pistas que aún no han sido compradas por nadie
+SELECT	T.TrackId,
+		T.Name
+FROM Track T
+LEFT JOIN InvoiceLine I 
+ON T.TrackId = I.TrackId
+WHERE I.TrackId IS NULL
+
+-- Listar los artistas que aún no han vendido ninguna pista ESTE NO ME FUNCIONA!!
+/* SELECT A.Name
+FROM Artist A
+LEFT JOIN Album AL 
+ON A.ArtistId = AL.ArtistId
+LEFT JOIN Track T 
+ON AL.AlbumId = T.AlbumId
+LEFT JOIN InvoiceLine I
+ON T.TrackId = I.TrackId
+WHERE I.InvoiceId IS NULL OR I.InvoiceLineId IS NULL
+*/
---
+++ consultas.script.sql
@@ -0,0 +1,129 @@
+-- Listar las pistas (tabla Track) con precio mayor o igual a 1€
+SELECT P.Name,
+		P.UnitPrice
+FROM Track P
+WHERE P.UnitPrice BETWEEN 0 AND 1
+
+-- Listar las pistas de más de 4 minutos de duración
+SELECT	T.Name,
+		T.Milliseconds
+FROM Track T
+WHERE T.Milliseconds BETWEEN 240000 AND 600000
+
+-- Listar las pistas que tengan entre 2 y 3 minutos de duración
+SELECT	T.Name,
+		T.Milliseconds
+FROM Track T
+WHERE T.Milliseconds BETWEEN 120000 AND 180000
+
+-- Listar las pistas que uno de sus compositores (columna Composer) sea Mercury
+SELECT	T.Name,
+		T.Composer
+FROM Track T
+WHERE T.Composer LIKE '%Mercury%'
+
+-- Calcular la media de duración de las pistas (Track) de la plataforma
+SELECT	AVG(T.Milliseconds) AS AvarageDuration
+FROM Track T
+
+-- Listar los clientes (tabla Customer) de USA, Canada y Brazil
+SELECT	C.FirstName,
+		C.LastName,
+		C.Country
+FROM Customer C
+WHERE C.Country IN ('USA', 'Canada', 'Brazil')
+
+-- Listar todas las pistas del artista 'Queen' (Artist.Name = 'Queen')
+SELECT	R.Name,
+		A.Title,
+		T.Name
+FROM Album A
+INNER JOIN Artist R
+ON R.ArtistID = A.ArtistId
+INNER JOIN Track T
+ON T.AlbumId = A.AlbumId
+WHERE R.Name = 'Queen'
+
+-- Listar las pistas del artista 'Queen' en las que haya participado como compositor David Bowie
+SELECT	R.Name,
+		A.Title,
+		T.Name
+FROM Album A
+INNER JOIN Artist R
+ON R.ArtistID = A.ArtistId
+INNER JOIN Track T
+ON T.AlbumId = A.AlbumId
+WHERE R.Name = 'Queen' AND T.Composer LIKE '%Bowie%'
+
+-- Listar las pistas de la playlist 'Heavy Metal Classic'
+SELECT	L.Name,
+		T.Name
+FROM PlaylistTrack P
+INNER JOIN Track T
+ON T.TrackId = P.TrackId
+INNER JOIN Playlist L
+ON L.PlaylistId = P.PlaylistId
+WHERE L.Name = 'Heavy Metal Classic'
+
+-- Listar las playlist junto con el número de pistas que contienen
+SELECT	L.Name,
+		Count(P.TrackId) AS NumberOfTracks
+FROM Playlist L
+INNER JOIN PlaylistTrack P
+ON L.PlaylistId = P.PlaylistId
+GROUP BY L.PlaylistId, L.Name
+
+-- Listar las playlist (sin repetir ninguna) que tienen alguna canción de AC/DC
+SELECT	DISTINCT(P.PlaylistId),
+		P.Name
+FROM Playlist P
+INNER JOIN PlaylistTrack PT
+ON PT.PlaylistId = P.PlaylistId
+INNER JOIN Track T
+ON T.TrackId = PT.TrackId
+INNER JOIN Album A
+ON A.AlbumId = T.AlbumId
+INNER JOIN Artist AR
+ON AR.ArtistId = A.ArtistId
+WHERE AR.Name = 'AC/DC'
+
+-- Listar las playlist que tienen alguna canción del artista Queen, junto con la cantidad que tienen
+SELECT	DISTINCT(P.PlaylistId),
+		COUNT(PT.TrackId) AS NumberOfTracks,
+		P.Name
+FROM Playlist P
+INNER JOIN PlaylistTrack PT
+ON PT.PlaylistId = P.PlaylistId
+INNER JOIN Track T
+ON T.TrackId = PT.TrackId
+INNER JOIN Album A
+ON A.AlbumId = T.AlbumId
+INNER JOIN Artist AR
+ON AR.ArtistId = A.ArtistId
+WHERE AR.Name = 'Queen'
+GROUP BY P.PlaylistId, P.Name
+
+-- Listar las pistas que no están en ninguna playlist
+SELECT	T.TrackId,
+		T.Name
+FROM Track T
+LEFT JOIN PlaylistTrack PT
+ON PT.TrackId = T.TrackId
+WHERE PT.PlaylistId IS NULL
+
+-- Listar los artistas que no tienen album
+SELECT	AR.ArtistId,
+		AR.Name
+FROM Artist AR
+LEFT JOIN Album AL
+ON AL.ArtistId = AR.ArtistId
+WHERE AL.AlbumId IS NULL
+
+-- Listar los artistas con el número de albums que tienen
+SELECT	DISTINCT(AR.ArtistId),
+		COUNT(AL.AlbumId) AS NumberOfAlbums,
+		AR.Name
+FROM Artist AR
+LEFT JOIN Album AL
+ON AR.ArtistId = AL.ArtistId
+GROUP BY AR.ArtistId, AR.Name
